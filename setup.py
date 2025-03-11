1c2ef6a3ac67fac98c14c7109da7ac7ac7e99c94
hucrosj <harold2352@gmail.com>
2025-03-11 12:47:20 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepositorio",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Harold Ucros",                         # Tu nombre
+    author_email="harold2352@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/hucrosj/tercerRepositorio",     # URL del proyecto
+)
